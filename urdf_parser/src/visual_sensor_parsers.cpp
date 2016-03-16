8f2acfbeee295a523661285ed4b63b6f425a954d
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2016-03-16 21:00:57 +0100

    simplified visual sensor parsing
    moved basic attribute parsing routine to utils.h

--- urdf_parser/src/visual_sensor_parsers.cpp
+++ urdf_parser/src/visual_sensor_parsers.cpp
@@ -35,12 +35,11 @@
 /* Author: John Hsu */
 
 #include "urdf_parser/visual_sensor_parsers.h"
+#include "urdf_parser/utils.h"
+#include "urdf_parser/pose.h"
 #include <urdf_sensor/camera.h>
 #include <urdf_sensor/ray.h>
-
-#include <boost/lexical_cast.hpp>
 #include <console_bridge/console.h>
-#include "urdf_parser/pose.h"
 
 namespace urdf {
 
@@ -51,110 +50,19 @@ SensorBaseSharedPtr CameraParser::parse(TiXmlElement &config)
   TiXmlElement *image = config.FirstChildElement("image");
   if (image)
   {
-    const char* width_char = image->Attribute("width");
-    if (width_char)
-    {
-      try
-      {
-        camera->width = boost::lexical_cast<unsigned int>(width_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Camera image width [%s] is not a valid int: %s", width_char, e.what());
-        return CameraSharedPtr();
-      }
+    try {
+      camera->width = parseAttribute<unsigned int>(*image, "width");
+      camera->height = parseAttribute<unsigned int>(*image, "height");
+      camera->format = parseAttribute<std::string>(*image, "format");
+      camera->hfov = parseAttribute<double>(*image, "hfov");
+      camera->near = parseAttribute<double>(*image, "near");
+      camera->far = parseAttribute<double>(*image, "far");
     }
-    else
+    catch (const std::exception &e)
     {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image width attribute");
+      CONSOLE_BRIDGE_logError("Camera sensor %s", e.what());
       return CameraSharedPtr();
     }
-
-    const char* height_char = image->Attribute("height");
-    if (height_char)
-    {
-      try
-      {
-        camera->height = boost::lexical_cast<unsigned int>(height_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Camera image height [%s] is not a valid int: %s", height_char, e.what());
-        return CameraSharedPtr();
-      }
-    }
-    else
-    {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image height attribute");
-      return CameraSharedPtr();
-    }
-
-    const char* format_char = image->Attribute("format");
-    if (format_char)
-      camera->format = std::string(format_char);
-    else
-    {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image format attribute");
-      return CameraSharedPtr();
-    }
-
-    const char* hfov_char = image->Attribute("hfov");
-    if (hfov_char)
-    {
-      try
-      {
-        camera->hfov = boost::lexical_cast<double>(hfov_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Camera image hfov [%s] is not a valid float: %s", hfov_char, e.what());
-        return CameraSharedPtr();
-      }
-    }
-    else
-    {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image hfov attribute");
-      return CameraSharedPtr();
-    }
-
-    const char* near_char = image->Attribute("near");
-    if (near_char)
-    {
-      try
-      {
-        camera->near = boost::lexical_cast<double>(near_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Camera image near [%s] is not a valid float: %s", near_char, e.what());
-        return CameraSharedPtr();
-      }
-    }
-    else
-    {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image near attribute");
-      return CameraSharedPtr();
-    }
-
-    const char* far_char = image->Attribute("far");
-    if (far_char)
-    {
-      try
-      {
-        camera->far = boost::lexical_cast<double>(far_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Camera image far [%s] is not a valid float: %s", far_char, e.what());
-        return CameraSharedPtr();
-      }
-    }
-    else
-    {
-      CONSOLE_BRIDGE_logError("Camera sensor needs an image far attribute");
-      return CameraSharedPtr();
-    }
-
   }
   else
   {
@@ -172,120 +80,32 @@ SensorBaseSharedPtr RayParser::parse(TiXmlElement &config)
   TiXmlElement *horizontal = config.FirstChildElement("horizontal");
   if (horizontal)
   {
-    const char* samples_char = horizontal->Attribute("samples");
-    if (samples_char)
-    {
-      try
-      {
-        ray->horizontal_samples = boost::lexical_cast<unsigned int>(samples_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray horizontal samples [%s] is not a valid float: %s", samples_char, e.what());
-        return RaySharedPtr();
-      }
+    try {
+      ray->horizontal_samples = parseAttribute<unsigned int>(*horizontal, "samples");
+      ray->horizontal_resolution = parseAttribute<double>(*horizontal, "resolution");
+      ray->horizontal_min_angle = parseAttribute<double>(*horizontal, "min_angle");
+      ray->horizontal_max_angle = parseAttribute<double>(*horizontal, "max_angle");
     }
-
-    const char* resolution_char = horizontal->Attribute("resolution");
-    if (resolution_char)
+    catch (const std::exception &e)
     {
-      try
-      {
-        ray->horizontal_resolution = boost::lexical_cast<double>(resolution_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray horizontal resolution [%s] is not a valid float: %s", resolution_char, e.what());
-        return RaySharedPtr();
-      }
-    }
-
-    const char* min_angle_char = horizontal->Attribute("min_angle");
-    if (min_angle_char)
-    {
-      try
-      {
-        ray->horizontal_min_angle = boost::lexical_cast<double>(min_angle_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray horizontal min_angle [%s] is not a valid float: %s", min_angle_char, e.what());
-        return RaySharedPtr();
-      }
-    }
-
-    const char* max_angle_char = horizontal->Attribute("max_angle");
-    if (max_angle_char)
-    {
-      try
-      {
-        ray->horizontal_max_angle = boost::lexical_cast<double>(max_angle_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray horizontal max_angle [%s] is not a valid float: %s", max_angle_char, e.what());
-        return RaySharedPtr();
-      }
+      CONSOLE_BRIDGE_logError("Ray horizontal: %s", e.what());
+      return RaySharedPtr();
     }
   }
 
   TiXmlElement *vertical = config.FirstChildElement("vertical");
   if (vertical)
   {
-    const char* samples_char = vertical->Attribute("samples");
-    if (samples_char)
-    {
-      try
-      {
-        ray->vertical_samples = boost::lexical_cast<unsigned int>(samples_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray vertical samples [%s] is not a valid float: %s", samples_char, e.what());
-        return RaySharedPtr();
-      }
+    try {
+      ray->vertical_samples = parseAttribute<unsigned int>(*vertical, "samples");
+      ray->vertical_resolution = parseAttribute<double>(*vertical, "resolution");
+      ray->vertical_min_angle = parseAttribute<double>(*vertical, "min_angle");
+      ray->vertical_max_angle = parseAttribute<double>(*vertical, "max_angle");
     }
-
-    const char* resolution_char = vertical->Attribute("resolution");
-    if (resolution_char)
+    catch (const std::exception &e)
     {
-      try
-      {
-        ray->vertical_resolution = boost::lexical_cast<double>(resolution_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray vertical resolution [%s] is not a valid float: %s", resolution_char, e.what());
-        return RaySharedPtr();
-      }
-    }
-
-    const char* min_angle_char = vertical->Attribute("min_angle");
-    if (min_angle_char)
-    {
-      try
-      {
-        ray->vertical_min_angle = boost::lexical_cast<double>(min_angle_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray vertical min_angle [%s] is not a valid float: %s", min_angle_char, e.what());
-        return RaySharedPtr();
-      }
-    }
-
-    const char* max_angle_char = vertical->Attribute("max_angle");
-    if (max_angle_char)
-    {
-      try
-      {
-        ray->vertical_max_angle = boost::lexical_cast<double>(max_angle_char);
-      }
-      catch (boost::bad_lexical_cast &e)
-      {
-        CONSOLE_BRIDGE_logError("Ray vertical max_angle [%s] is not a valid float: %s", max_angle_char, e.what());
-        return RaySharedPtr();
-      }
+      CONSOLE_BRIDGE_logError("Ray horizontal: %s", e.what());
+      return RaySharedPtr();
     }
   }
   return ray;
